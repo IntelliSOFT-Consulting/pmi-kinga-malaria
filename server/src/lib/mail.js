ab9e6adcf7e3b1ae2f7d19002eab847b2aff1122
Patrick Nyatindo <nyatindopatrick@gmail.com>
2023-03-27 09:36:07 +0300

    Add outlook mail

--- server/src/lib/mail.js
+++ server/src/lib/mail.js
@@ -1,12 +1,33 @@
 import { config } from 'dotenv';
 import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 
 config();
 
-sgMail.setApiKey(process.env.EMAIL_PASS);
+const { MAIL_PASS, EMAIL, EMAIL_PASS } = process.env;
+
+sgMail.setApiKey(EMAIL_PASS);
+
+const transporter = nodemailer.createTransport({
+  service: 'outlook',
+  auth: {
+    user: EMAIL,
+    pass: MAIL_PASS,
+  },
+});
 
 export default async function sendEmail(to, subject, text, attachments = null) {
   try {
+    if (MAIL_PASS) {
+      const send = await transporter.sendMail({
+        from: `PMI Kinga Malaria <${EMAIL}>`,
+        to,
+        subject,
+        text,
+        attachments,
+      });
+      return send;
+    }
     const msg = {
       to,
       from: 'PMI Kinga Malaria <mail@pmi-kinga-malaria.org>',
@@ -16,6 +37,7 @@ export default async function sendEmail(to, subject, text, attachments = null) {
     };
 
     await sgMail.send(msg);
+    return;
   } catch (error) {
     console.log(error);
     return error;
