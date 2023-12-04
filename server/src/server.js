26d9b72544ce0b4239effc6fcef8df5780dc3b95
Patrick Nyatindo <nyatindopatrick@gmail.com>
2023-12-04 12:35:39 +0300

    add cron for supervisors

--- server/src/server.js
+++ server/src/server.js
@@ -45,9 +45,9 @@ cron.schedule('*/1 * * * *', () => {
   sendReports();
 });
 
-main();
-// cron.schedule('*/30 * * * *', () => {
-// });
+cron.schedule('0 */2 * * *', () => {
+  main();
+});
 
 app.listen(Port, () => {
   console.log(`Server is running on port ${Port}`);
